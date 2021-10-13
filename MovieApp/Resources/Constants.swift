62181862aea14363911d49b8059253e6237300a4
Heba Ali <hebaaliali18@gmail.com>
2021-10-13 15:03:38 +0200

    Init Project Structure

---
+++ MovieApp/Resources/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  MovieApp
+//
+//  Created by Heba on 13/10/2021.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let appName = "Movies App"
+    static let APIKEY = ""
+    
+    struct Cells {
+        static let homeCell = "HomeCell"
+    }
+}
